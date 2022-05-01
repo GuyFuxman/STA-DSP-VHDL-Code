dc35086b3ae8bd31c33bb2228baf6f5e91c68e78
GuyFuxman <101499605+GuyFuxman@users.noreply.github.com>
2022-05-01 15:35:19 +0300

    Add files via upload

--- HPS_Converter.vhd
+++ HPS_Converter.vhd
@@ -7,12 +7,13 @@ Entity HPS_Converter is
 	Generic(FFT_In_Data_Width       : integer   := 16;
 			  FFT_Transform_Width     : integer   := 9;
 			  FFT_Max_Transform_Width : integer   := 9;
-			  MsbWidth                : integer   := 32);
+			  MsbWidth                : integer   := 32;
+			  Fraction_Size           : integer   := 3);
 
 
-   port( i_Squared_Data         : in std_logic_vector ((MsbWidth - 1) downto 0);
+   port( i_Data_To_Logger         : in std_logic_vector ((MsbWidth - 1) + (Fraction_Size - 1) downto 0);
 			i_Bit_Reversed_Counter : in std_logic_vector ((FFT_Transform_Width - 1) downto 0);
-		   O_Data_to_HPS          : out std_logic_vector (((MsbWidth * 2) - 1) downto 0)
+		   O_Data_to_HPS          : out std_logic_vector (((MsbWidth * 2) - 1) + (Fraction_Size - 1) downto 0)
 		 );
 end Entity;
 
@@ -20,7 +21,7 @@ Architecture Arch_HPS_Converter of HPS_Converter is
 
 begin  
 
-	o_Data_to_HPS(((MsbWidth * 2) - 1) downto MsbWidth) <= i_Squared_Data;
+	o_Data_to_HPS(((MsbWidth * 2) - 1 + (Fraction_Size - 1)) downto MsbWidth) <= i_Data_To_Logger;
 	o_Data_to_HPS((MsbWidth - 1) downto FFT_Transform_Width) <= (others => '0');
 	o_Data_to_HPS((FFT_Transform_Width - 1) downto 0) <= i_Bit_Reversed_Counter;
  
